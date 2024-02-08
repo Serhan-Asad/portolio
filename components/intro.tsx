7b7a06019425324cbb734efca1dcd2c12e4d49b2
Serhan <serhan@MacBook-Pro-1264.local>
2024-02-08 01:31:41 -0500

    fixed typo

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import picture from "@/public/headshot.jpeg"
+import picture from "@/public/headShot.jpeg"
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
